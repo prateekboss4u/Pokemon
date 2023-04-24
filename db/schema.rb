6b40727bfe3c68137076ab106aeeed399f3d2c97
Ajinkya Salunkhe <salunkheajinkya07@gmail.com>
2023-04-24 16:20:52 +0530

    Add User Model
    create user model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_094628) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_104945) do
   create_table "all_pokemons", force: :cascade do |t|
     t.string "name"
     t.string "strength"
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_094628) do
     t.string "name"
     t.integer "total_pokemon"
     t.integer "victory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
